ab7c1b23b854d4ef2ef464943df8144825b116cb
JaowadH <jaowad.hossain@keyin.com>
2025-08-14 18:50:17 -0230

    Harden TreeRequest deserialization (aliases, null-safe)

--- src/main/java/com/example/bst/dto/TreeRequest.java
+++ src/main/java/com/example/bst/dto/TreeRequest.java
@@ -1,27 +1,31 @@
 package com.example.bst.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
+/**
+ * Accepts any of these payloads:
+ * {"numbers":[8,3,10,1,6],"balanced":true}
+ * {"values":[8,3,10,1,6],"balanced":true}
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
-// Accepts {"numbers":[...], "balanced": true}
 public class TreeRequest {
+    @JsonProperty("numbers")              // canonical name
+    @JsonAlias({"values"})                // also accept `values`
     private List<Integer> numbers;
-    private boolean balanced;
 
-    public TreeRequest() {}                 // required for Jackson
+    @JsonProperty("balanced")
+    @JsonAlias({"balance","isBalanced"})  // accept a few variants
+    private Boolean balanced = Boolean.FALSE;
 
-    public List<Integer> getNumbers() {     // getters/setters required
-        return numbers;
-    }
-    public void setNumbers(List<Integer> numbers) {
-        this.numbers = numbers;
-    }
+    public TreeRequest() {}               // required by Jackson
 
-    public boolean isBalanced() {
-        return balanced;
-    }
-    public void setBalanced(boolean balanced) {
-        this.balanced = balanced;
-    }
+    public List<Integer> getNumbers() { return numbers; }
+    public void setNumbers(List<Integer> numbers) { this.numbers = numbers; }
+
+    public boolean isBalanced() { return Boolean.TRUE.equals(balanced); }
+    public void setBalanced(Boolean balanced) { this.balanced = balanced; }
 }
