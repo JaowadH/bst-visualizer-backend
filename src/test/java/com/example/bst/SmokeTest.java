e82cc342888671a5f3ae857276c34efe5c58eb43
JaowadH <jaowad.hossain@keyin.com>
2025-08-14 18:09:32 -0230

    Backend ready for Render

---
+++ src/test/java/com/example/bst/SmokeTest.java
@@ -0,0 +1,8 @@
+package com.example.bst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest @ActiveProfiles("test")
+class SmokeTest { @Test void contextLoads() {} }
